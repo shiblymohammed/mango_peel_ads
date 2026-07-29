08d41648a66d762734b0b4cfb8c16401f695ffb7
Shaamil777 <shamilm2324@gmail.com>
2026-07-29 16:57:08 +0530

    feat:hover effect trigger removed from mobile and tablet sizes wihtout mouse cursor

--- src/components/team/TeamGrid.tsx
+++ src/components/team/TeamGrid.tsx
@@ -55,7 +55,7 @@ const teamMembers = [
 
 export default function TeamGrid() {
     return (
-        <div className="relative z-10 flex flex-wrap justify-center gap-x-8 sm:gap-x-14 md:gap-x-20 lg:gap-x-24 gap-y-20 sm:gap-y-36 md:gap-y-28 max-w-[1400px] w-full mt-16 md:mt-24 px-4">
+        <div className="relative z-10 flex flex-wrap justify-center gap-x-8 sm:gap-x-14 md:gap-x-20 lg:gap-x-24 gap-y-28 sm:gap-y-36 md:gap-y-28 max-w-[1400px] w-full mt-16 md:mt-24 px-4">
             {teamMembers.map((member, index) => (
                 <div 
                     key={index} 
